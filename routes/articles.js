ecdc9f23a59727d261faf0c327ce964d55f25bbb
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-12-10 17:23:00 -0500

    added articles route

--- routes/articles.js
+++ routes/articles.js
@@ -1,39 +1,88 @@
 const express = require('express');
-const {Comments} = require('./comments');
+const {CommentsRouter} = require('./comments');
 const { BrandsRouter } = require('./brands');
+const {Article, CArticle } = require('../models');
 
 
-const Articles = express.Router();
-Articles.use('/comments', Comments);
-Articles.use('/brands', BrandsRouter);
+const ArticlesRouter = express.Router();
+ArticlesRouter.use('/comments', CommentsRouter);
+ArticlesRouter.use('/brands', BrandsRouter);
 
 // GET http://localhost:3001/articles/
-Articles.get('/', (req, res) => {
+ArticlesRouter.get('/', async (req, res) => {
   //const articles = a
-  res.json({msg: 'get all articles'});
+  try{
+    const articles = await Article.findAll();
+    console.log(articles)
+    res.json({articles});
+  } catch(evt) {
+    res.status(500).json({
+      msg: evt.messsage
+    })
+  }
 });
 
 // POST http://localhost:3001/articles/
-Articles.post('/', (req, res) => {
-
-  res.json({msg: 'post a article'});
+ArticlesRouter.post('/', async (req, res) => {
+  try{
+    const articles = await Article.create(req.body);
+    res.json({articles});
+  } catch(evt) {
+    res.status(500).json({
+      msg: evt.messsage
+    })
+  }
 });
 
 // GET http://localhost:3001/articles/:article_id
-Articles.get('/:article_id', (req, res) => {
-  res.json({msg: `get article by id ${req.params.article_id}`});
+ArticlesRouter.get('/:article_id', async (req, res) => {
+  try{
+    const articles = await Article.findByPk(req.params.article_id);
+    res.json({articles})
+  } catch(evt) {
+    res.status(500).json({
+      msg: evt.messsage
+    })
+  }
 });
 
 // PUT http://localhost:3001/articles/:article_id
-Articles.put('/:article_id', (req, res) => {
-  res.json({msg: `update article by id ${req.params.article_id}`});
+ArticlesRouter.put('/:article_id', async (req, res) => {
+  // res.json({msg: `update article by id ${req.params.article_id}`});
+  try{
+    const data = req.body;
+    const article = await Article.findByPk(req.params.article_id);
+    const resp = await Article.update({
+      title: data.title || article.title,
+    },
+    {
+      where: {
+        id: req.params.article_id,
+      }
+    });
+    console.log(data)
+    res.json(data);
+  }
+  catch(evt) {
+    res.status(500).json({msg: evt.message})
+  }
 });
 
 // DELETE http://localhost:3001/articles/:article_id
-Articles.delete('/:article_id', (req, res) => {
-  res.json({msg: `delete article by id ${req.params.article_id}`});
+ArticlesRouter.delete('/:article_id', async (req, res) => {
+  // res.json({msg: `delete article by id ${req.params.article_id}`});
+  try{
+    await Article.destroy({
+      where: {
+        id: req.params.article_id,
+      }
+    },{truncate: true});
+    res.json({msg: `delete article by id ${req.params.article_id}`});
+  } catch(evt) {
+    res.status(500).json({msg: evt.message})
+  }
 });
 
 module.exports = {
-  Articles,
+  ArticlesRouter,
 }
--- routes/comments.js
+++ routes/comments.js
@@ -1,32 +1,33 @@
 const express = require('express');
+const { Comment, CComment } = require('../models');
 
-const Comments = express.Router();
+const CommentsRouter = express.Router();
 
 // http://localhost:3001/comments
-Comments.get('/', (req, res) => {
+CommentsRouter.get('/', (req, res) => {
   res.json({msg: 'get all comments'});
 });
 
-// POST http://localhost:3001/comments/
-Comments.post('/', (req, res) => {
-  res.json({msg: 'post a comment'});
-});
-
-// GET http://localhost:3001/comments/:comment_id
-Comments.get('/:comment_id', (req, res) => {
-  res.json({msg: `get comment by id ${req.params.comment_id}`});
-});
-
-// PUT http://localhost:3001/comments/:comment_id
-Comments.put('/:comment_id', (req, res) => {
-  res.json({msg: `update comment by id ${req.params.comment_id}`});
-});
-
-// DELETE http://localhost:3001/comments/:comment_id
-Comments.delete('/:comment_id', (req, res) => {
-  res.json({msg: `delete comment by id ${req.params.comment_id}`});
-});
+// // POST http://localhost:3001/comments/
+// CommentsRouter.post('/', (req, res) => {
+//   res.json({msg: 'post a comment'});
+// });
+//
+// // GET http://localhost:3001/comments/:comment_id
+// CommentsRouter.get('/:comment_id', (req, res) => {
+//   res.json({msg: `get comment by id ${req.params.comment_id}`});
+// });
+//
+// // PUT http://localhost:3001/comments/:comment_id
+// CommentsRouter.put('/:comment_id', (req, res) => {
+//   res.json({msg: `update comment by id ${req.params.comment_id}`});
+// });
+//
+// // DELETE http://localhost:3001/comments/:comment_id
+// CommentsRouter.delete('/:comment_id', (req, res) => {
+//   res.json({msg: `delete comment by id ${req.params.comment_id}`});
+// });
 
 module.exports = {
-  Comments,
+  CommentsRouter,
 }
