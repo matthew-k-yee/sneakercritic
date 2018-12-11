ef48a6c1f99746e048de9f2f95bfb472d062b3c2
Seth <st.to.1989@gmail.com>
2018-12-11 10:13:32 -0500

    Made changes related to implementing react-router-dom in Client/App.js and Client/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import BrandsList from './components/Brands/List';
@@ -9,7 +10,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'welcome',
       brands: [],
     }
   }
@@ -25,20 +25,15 @@ class App extends Component {
     return await axios.get(`${BASE_URL}/brands`).then(data => data.data.brands);
   }
 
-  getView = () => {
-    switch(this.state.view) {
-      case 'welcome': return ("Welcome!");
-      case 'index':   return ("Index!");
-      case 'brands':  return ("Brands!");
-      default:        return ("404");
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        {this.getView()}
         <button onClick={() => {this.setState({view: 'index'})}}>Click me!</button>
+        <div className="view">
+          <Switch>
+            <Route exact path='/' component={Home}/>
+          </Switch>
+        </div>
         <BrandsList brands={this.state.brands} />
       </div>
     );
