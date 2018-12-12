0a9e3df3f8e1245a4c89465d95afcf28f8450974
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-12-12 10:16:02 -0500

    render full page article

---
+++ client/src/components/Article/CommentItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function CommentItem(props) {
+
+  return(
+    <div>
+      <h3>{props.comment.title}</h3>
+      <p>{props.comment.text}</p>
+      <p>User's Score: {props.comment.user_score}</p>
+    </div>
+  )
+}
