0a59f507430697dc1633c29b60568627956f24e7
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-12-13 11:02:49 -0500

    style register form

--- client/src/components/Register/Index.js
+++ client/src/components/Register/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Register.css';
 import { Link } from 'react-router-dom';
 import RegisterForm from "./RegisterForm";
 
 export default function Register(props) {
   return (
-    <div>
+    <div className = 'RegisterForm'>
       <h1>Register</h1>
       <RegisterForm onChange={props.onChange} onSubmit={props.onSubmit} {...props}/>
       <Link to={'login'}>Go Login!</Link>
