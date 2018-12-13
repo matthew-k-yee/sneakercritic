8c06c1ae7f8484c80868597c9383ab2a2c4291d2
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-12-13 11:24:37 -0500

    css for login form

--- client/src/components/Login/Index.js
+++ client/src/components/Login/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Login.css'
 import LoginForm from "./LoginForm";
 import { Link } from 'react-router-dom';
 
 export default function Login(props) {
   return (
-    <div>
+    <div className='LoginForm'>
       <h1>Login</h1>
       <LoginForm onChange={props.onChange} onSubmit={props.onSubmit} {...props} />
       <Link to={'register'}><h2>Go to Register</h2></Link>
